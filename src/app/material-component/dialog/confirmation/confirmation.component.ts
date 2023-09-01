524ba72158259543f9ded118663e88d63f3c0dab
klajdi.hasani <hasaniklajdi17@gmail.com>
2023-09-01 16:50:48 +0200

    Added dashboard, guards and interceptors on angular

---
+++ src/app/material-component/dialog/confirmation/confirmation.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation',
+  templateUrl: './confirmation.component.html',
+  styleUrls: ['./confirmation.component.scss']
+})
+export class ConfirmationComponent implements OnInit {
+
+  onEmitStatusChange = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
